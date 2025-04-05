e75238d1ade6e6fa0428d3b12d0b8ea004223f47
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-04-05 13:54:07 +0200

    Add API utility for fetching music data

---
+++ utils/api.js
@@ -0,0 +1,26 @@
+const apiKey = '67f1141db38f06160cb55ffd';
+const apiUrl = 'https://playlistdata-a6f6.restdb.io/rest/musics';
+
+async function fetchMusics() {
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-apikey': apiKey,
+            },
+        });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+
+}
+
+fetchMusics();
