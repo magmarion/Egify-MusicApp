6188935d35099d3ef2cf0ee065af8460b90c0ed8
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-04-07 17:20:33 +0200

    Implement event listeners for hamburger menu and navigation links to enhance user interaction

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 import { initMusicApp } from "./controller/musicController.js";
 
 initMusicApp();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const nav = document.querySelector("nav");
+    const links = document.querySelectorAll("nav a");
+
+    hamburger.addEventListener("click", () => {
+        nav.classList.toggle("active");
+        document.body.classList.toggle("menu-open");
+    });
+
+    links.forEach(link => {
+        link.addEventListener("click", () => {
+            nav.classList.remove("active");
+            document.body.classList.remove("menu-open");
+        });
+    });
+});
