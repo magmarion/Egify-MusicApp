76f025850167aedf6bd6348213685321d715f749
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-04-19 18:39:48 +0200

    Update mock music data: add new tracks and correct genre for existing entry

--- utils/mockedData.js
+++ utils/mockedData.js
@@ -1,11 +1,35 @@
 export const mockMusics = [
     {
+        title: "Wavin' Flag",
+        artist: "K'naan",
+        genre: "Reggae fusion",
+        releaseYear: "2009",
+        trackUrl: "https://youtu.be/WTJSt4wP2ME?si=eikQh3GgU_blE37q",
+        coverImage: "https://i1.sndcdn.com/artworks-dCkiH4AhlygHY4F0-hKA6lg-t500x500.jpg"
+    },
+    {
         title: "Faded",
         artist: "Alan Walker",
         genre: "Dance Electronoic",
         releaseYear: "2015",
         trackUrl: "https://youtu.be/kId1v7JDJtY",
         coverImage: "https://i1.sndcdn.com/artworks-000166082795-l5qemj-t500x500.jpg"
+    },
+    {
+        title: "Calm Down",
+        artist: "Rema",
+        genre: "Afrobeats",
+        releaseYear: "2022",
+        trackUrl: "https://youtu.be/h1iXtgCOCxo?si=fujn8wEaG2ITGXLh",
+        coverImage: ""
+    },
+    {
+        title: "Shape of You",
+        artist: "Ed Sheeran",
+        genre: "Pop",
+        releaseYear: "2017",
+        trackUrl: "https://youtu.be/JGwWNGJdvx8?si=pRkvSJ5Lnpucoi9g",
+        coverImage: "https://cloud.rtl.it/RTLFM/News/Article/1000x1000/ed-sheeran-ecco-il-nuovo-video-shape-of-you-j1dug.png"
     },
     {
         title: "Spectre",
@@ -18,7 +42,7 @@ export const mockMusics = [
     {
         title: "On The FLoor",
         artist: "Jennifer Lopez ft. Pitbull",
-        genre: "Dance Pop/Techno",
+        genre: "Dance Techno",
         releaseYear: "2011",
         trackUrl: "https://youtu.be/t4H_Zoh7G5A?si=iXZhikkAPepdcLgg",
         coverImage: "https://www.salutemusic.com/cdn/shop/products/Nails_Hair_Hips_Heels-8.png?v=1658940737"
