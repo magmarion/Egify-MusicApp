d2a3a170e284b342469ecfcbf051ca4817f5c071
elnurguliy <180274282+elnurguliy@users.noreply.github.com>
2025-06-18 10:53:39 +0200

    Add musics.json file and update fetchMusics to use local data

--- utils/api.js
+++ utils/api.js
@@ -1,19 +1,33 @@
-const apiKey = '67f1141db38f06160cb55ffd';
-const apiUrl = 'https://playlistdata-a6f6.restdb.io/rest/musics';
+// const apiKey = '67f1141db38f06160cb55ffd';
+// const apiUrl = 'https://playlistdata-a6f6.restdb.io/rest/musics';
+
+// export async function fetchMusics() {
+//     try {
+//         const response = await fetch(apiUrl, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'x-apikey': apiKey,
+//             },
+//         });
+//         if (!response.ok) throw new Error('Network response was not ok');
+//         return await response.json();
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//         return [];
+//     }
+// }
+
+
+const localDataUrl = 'assets/musics.json';
 
 export async function fetchMusics() {
     try {
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-apikey': apiKey,
-            },
-        });
+        const response = await fetch(localDataUrl);
         if (!response.ok) throw new Error('Network response was not ok');
         return await response.json();
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching local JSON data:', error);
         return [];
     }
 }
